c0e02c4b077fc97f28ac94956b909b352f79d742
GuidoCostaDF <guidocostadf@gmail.com>
2022-08-23 11:54:25 -0300

    aplicando property binding no componente nova-transferencia

--- bytebank/src/app/app.module.ts
+++ bytebank/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,8 +1,20 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector:'app-nova-transferencia',
-    templateUrl: './nova-transferencia.component.html',
-    styleUrls: ['./nova-transferencia.component.scss']
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
-export class NovaTransferenciaComponent {}
+export class NovaTransferenciaComponent {
+
+  valor: number;
+  destino: number;
+
+  transferir() {
+    console.log('nova transferencia solicitada');
+    console.log(this.valor);
+    console.log(this.destino);
+  }
+
+
+}
